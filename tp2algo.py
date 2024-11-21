b239d74c539a2d05534cfbbb82d86a3dd7871c32
chaimachai12 <163171246+chaimachai12@users.noreply.github.com>
2024-11-21 15:19:42 +0100

    Update tp2algo.py

--- tp2algo.py
+++ tp2algo.py
@@ -1,42 +1,6 @@
-#la fonction tamiser(question 1)
-def tamiser(T, ipere, ifin):
-    
-    continuer = True
-    ech = False
-
-    while continuer:
-        continuer = False
-        ifils = 2 * ipere  
-
-
-        if ifils < ifin and T[ifils + 1] > T[ifils]:
-            ifils += 1
-
-        if ifils <= ifin and T[ipere] < T[ifils]:
-            T[ipere], T[ifils] = T[ifils], T[ipere]
-            ech = True
-            continuer = True
-            ipere = ifils 
-    return ech
-
-def construire_tas(T):
-
-    n = len(T) - 1  
-  
-    for i in range(n // 2, 0, -1):
-        tamiser(T, i, n)
-arbre = [10, 22, 5, 18, 20, 25, 40, 30, 35, 12]  
-print("Arbre initial :", arbre[1:])
-construire_tas(arbre)
-print("TAS max :", arbre[1:])
-
-
-
-# tri par tas (question 2)
+# tri par tas 
 def entasser(tab, i, n):
-    """
-    Fonction pour réorganiser le tas à partir de l'index i.
-    """
+
     gauche = 2 * i + 1  
     droite = 2 * i + 2  
     plus_grand = i
